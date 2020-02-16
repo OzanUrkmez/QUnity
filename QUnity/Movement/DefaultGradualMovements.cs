37751d3b7e2f30e81613f9c05cbf5bb23e9b1d7f
Ozan Ürkmez <arkantos250@gmail.com>
2020-02-16 11:17:03 +0300

    define starting variables

--- QUnity/Movement/DefaultGradualMovements.cs
+++ QUnity/Movement/DefaultGradualMovements.cs
@@ -14,31 +14,84 @@ namespace QUnity.Movement
     public class QGradualEllipticalMovement : QIGradualMovement
     {
 
-        private Vector3 initial, final, circularPivot;
-        private float xCoeff, yCoeff;
+        private Vector3 initial, final, pivot;
+        private float degree, xCoeff, yCoeff;
         private float movementTime;
-        private float currentTime;
+        private bool movementStacked;
+        private float currentTime = -1;
         private GameObject movedObject;
         private Rigidbody rigidbody;
+        private bool isRigidBodyMovement = false;
 
         //TODO Ability to transform already created ellipsis for reuse? 
+
+        #region Construction
 
         /// <summary>
         /// Initializes the elliptical movement
         /// </summary>
         /// <param name="gameObject"> The gameobject to be moved. </param>
-        /// <param name="startingPosition"> The starting position. If set to  </param>
-        /// <param name="finalPosition"></param>
-        /// <param name="time"></param>
+        /// <param name="startingPosition"> The starting position. If set to any other position than that of the gameobject, the gameobject will be moved to that position when the movement starts. </param>
+        /// <param name="finalPosition"> The position the gameobject will be moving towards. </param>
+        /// <param name="time"> The time it shall take for the movement to finish. </param>
         /// <param name="stacked"> Defines whether the gradual movement should be stacked along with a series of movements; or only start when a series of movements has finished and define its own series of movements upon which other movements may stack. </param>
         /// <param name="referencePivot"></param>
         /// <param name="degree"> </param>
         public QGradualEllipticalMovement(GameObject gameObject, Vector3 startingPosition, Vector3 finalPosition, Vector3 referencePivot, float degree , float time, bool stacked)
         {
-
+            movedObject = gameObject;
+            initial = startingPosition;
+            final = finalPosition;
+            pivot = referencePivot;
+            movementTime = time;
+            this.degree = degree;
+            movementStacked = stacked;
         }
 
+        /// <summary>
+        /// Initializes the elliptical movement
+        /// </summary>
+        /// <param name="gameObject"> The gameobject to be moved. </param>
+        /// <param name="startingPosition"> The starting position. If set to any other position than that of the gameobject, the gameobject will be moved to that position when the movement starts. </param>
+        /// <param name="finalPosition"> The position the gameobject will be moving towards. </param>
+        /// <param name="time"> The time it shall take for the movement to finish. </param>
+        /// <param name="stacked"> Defines whether the gradual movement should be stacked along with a series of movements; or only start when a series of movements has finished and define its own series of movements upon which other movements may stack. </param>
+        /// <param name="referencePivot"></param>
+        /// <param name="degree"> </param>
+        public QGradualEllipticalMovement(Rigidbody rigidbody, Vector3 startingPosition, Vector3 finalPosition, Vector3 referencePivot, float degree, float time, bool stacked)
+        {
+            isRigidBodyMovement = true;
+            this.rigidbody = rigidbody;
+            initial = startingPosition;
+            final = finalPosition;
+            pivot = referencePivot;
+            movementTime = time;
+            this.degree = degree;
+            movementStacked = stacked;
+        }
+
+        #endregion
+
+
         #region Manager Communication and Manager Related Properties
+
+        /// <summary>
+        /// Starts the defined movement if it has not already been started.
+        /// </summary>
+        public void StartMovement()
+        {
+            if (currentTime > 0)
+                return;
+            if (isRigidBodyMovement)
+            {
+                //rigidbody movement
+            }
+            else
+            {
+                //regular movement.
+                movedObject.transform.position = initial;
+            }
+        }
 
         /// <summary>
         /// whether the gradual movement should be stacked along with a series of movements; or only start when a series of movements has finished and define its own series of movements upon which other movements may stack.
