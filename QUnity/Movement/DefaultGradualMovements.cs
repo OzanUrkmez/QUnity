885abc797924613a10218fb1910931b14cc6b6ea
Ozan Ürkmez <arkantos250@gmail.com>
2020-02-16 08:51:05 +0300

    reformat

--- QUnity/Movement/DefaultGradualMovements.cs
+++ QUnity/Movement/DefaultGradualMovements.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 
 namespace QUnity.Movement
 {
-        //event systems etc as well. speed coefficient marks.
+    //event systems etc as well. speed coefficient marks.
     /// <summary>
     /// Defines an elliptical gradual movement integrated into the Questry Gradual Movement System. 
     /// </summary>
@@ -18,22 +18,38 @@ namespace QUnity.Movement
         private float xCoeff, yCoeff;
         private float movementTime;
         private GameObject movedObject;
+        private Rigidbody rigidbody;
+
+        //TODO Ability to transform already created ellipsis for reuse? 
 
         /// <summary>
-        /// Initializes the elliptical movement as being a 90 degree rotation.
+        /// Initializes the elliptical movement
         /// </summary>
         /// <param name="gameObject"> The gameobject to be moved. </param>
         /// <param name="startingPosition"> The starting position. If set to  </param>
         /// <param name="finalPosition"></param>
         /// <param name="time"></param>
-        public QGradualEllipticalMovement(GameObject gameObject, Vector3 startingPosition, Vector3 finalPosition, float time)
+        /// <param name="stacked"> Defines whether the gradual movement should be stacked along with a series of movements; or only start when a series of movements has finished and define its own series of movements upon which other movements may stack. </param>
+        /// <param name="referencePivot"></param>
+        /// <param name="degree"> </param>
+        public QGradualEllipticalMovement(GameObject gameObject, Vector3 startingPosition, Vector3 finalPosition, Vector3 referencePivot, float degree , float time, bool stacked)
         {
 
         }
 
-        #region Manager Communication
+        #region Manager Communication and Manager Related Properties
+
+        /// <summary>
+        /// whether the gradual movement should be stacked along with a series of movements; or only start when a series of movements has finished and define its own series of movements upon which other movements may stack.
+        /// </summary>
+        public bool MovementIsStacked { get; set; }
 
         public bool AttemptMerge(QIGradualMovement mov)
+        {
+            throw new NotImplementedException();
+        }
+
+        public float GetTimeLeft()
         {
             throw new NotImplementedException();
         }
@@ -43,9 +59,13 @@ namespace QUnity.Movement
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Returns whether the gradual movement should be stacked along with a series of movements; or only start when a series of movements has finished and define its own series of movements upon which other movements may stack.
+        /// </summary>
+        /// <returns>true for stacked, false for not stacked.</returns>
         public bool IsStacked()
         {
-            throw new NotImplementedException();
+            return MovementIsStacked;
         }
 
         public void OnMovementFinish(bool premature)
